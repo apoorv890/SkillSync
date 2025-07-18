f78128caf5ddf9182f6e978fa416419ef45e5a89
apoorv890 <apoorvb890@gmail.com>
2025-07-18 14:30:00 +0530

    feat(backend): create server entry

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import app from './src/app.js';
+import logger from './src/config/logger.js';
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Graceful shutdown
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    logger.info('HTTP server closed');
+    process.exit(0);
+  });
+});
+
+process.on('SIGINT', () => {
+  logger.info('SIGINT signal received: closing HTTP server');
+  server.close(() => {
+    logger.info('HTTP server closed');
+    process.exit(0);
+  });
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  logger.error('Unhandled Promise Rejection:', err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+export default server;
