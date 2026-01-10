9a551339a15baaa8fad58631e853853864dd4f4f
apoorv890 <apoorvb890@gmail.com>
2026-01-10 22:06:19 +0530

    fix(server): remove duplicate default export and shutdown handlers

--- server/server.js
+++ server/server.js
@@ -64,30 +64,3 @@ const setupGracefulShutdown = () => {
 startServer();
 
 export default server;
-
-// Graceful shutdown
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM signal received: closing HTTP server');
-  server.close(() => {
-    logger.info('HTTP server closed');
-    process.exit(0);
-  });
-});
-
-process.on('SIGINT', () => {
-  logger.info('SIGINT signal received: closing HTTP server');
-  server.close(() => {
-    logger.info('HTTP server closed');
-    process.exit(0);
-  });
-});
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err) => {
-  logger.error('Unhandled Promise Rejection:', err);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-export default server;
